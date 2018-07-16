90ac609c3eb2b7aa91f4ec8b858e73cfd50aa8dc
Udo Groebner <udo@groebnerweb.de>
2018-07-16 11:45:07 +0200

    We can directly set the content. Also print is default setting.

--- src/converter.mjs
+++ src/converter.mjs
@@ -1,6 +1,7 @@
 import showdown from "showdown";
 import handlebars from "handlebars";
 import puppeteer from "puppeteer";
+import juice from "juice";
 
 export async function handlebarsConvert(text, context = {}) {
   const template = handlebars.compile(text);
@@ -15,14 +16,8 @@ export async function markdownConvert(text, options = {}) {
 export async function pdfConvert(html, options = {}) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  await page.setContent(html);
   try {
-    await page.setRequestInterception(true);
-    page.once("request", request => {
-      request.respond({ body: html });
-      page.on("request", request => request.continue());
-    });
-    await page.goto("http://example.com");
-    await page.emulateMedia("print");
     return await page.pdf(options);
   } catch (e) {
     return Promise.reject(e);
